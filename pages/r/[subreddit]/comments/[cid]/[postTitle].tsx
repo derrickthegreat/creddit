bd9fd14f3028be7df10304f2be69c937cb2d2c39
Derrick A <0xdnero@proton.me>
2022-08-27 20:40:52 -0400

    add comment & replies to post page

--- pages/r/[subreddit]/comments/[cid]/[postTitle].tsx
+++ pages/r/[subreddit]/comments/[cid]/[postTitle].tsx
@@ -6,20 +6,34 @@ import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import Link from "next/link";
 import { GetServerSideProps } from "next";
 import Image from "next/image";
-import { marked } from 'marked'
+import { marked } from 'marked';
+import Comment from "../../../../../components/Comment";
 
 export default function PostComments(props: { post: any, comments: any }): JSX.Element {
     const post = props.post;
-    const comments = props.comments;
-
+    const comments = props.comments.map((comment: any, index: number) => {
+        //if(index > 100) { return }
+        // const replies = comment.replies.data.children;
+        return {
+            body: comment.body,
+            author: comment.author,
+            replies: comment.replies ? comment.replies.data.children.map(reply => reply.data) : null,            
+        }
+    });
+    console.log(comments);
+    const subreddit = post.subreddit_name_prefixed;
     return (
         <>
-            <Typography variant="h2">{post.subreddit}</Typography>
+            <Typography variant="h2"><Link href={`/${subreddit}`}>{subreddit}</Link></Typography>
+            <Box sx={{
+                display: "flex",
+                flexFlow: "column",
+                backgroundColor: "grey.100",
+            }}>
             <Box sx={{
                 display: 'flex',
                 gap: 1,
                 p: "10px",
-                backgroundColor: "grey.100",
                 m: "5px",
                 alignItems: 'center',
             }}>
@@ -43,7 +57,17 @@ export default function PostComments(props: { post: any, comments: any }): JSX.E
                         <Typography variant="caption">{`${post.num_comments} comments sorted by Best`}</Typography>
                     </Box>
                 </Stack>
-                <Button variant="text" sx={{ alignSelf: 'start'}} startIcon={<FavoriteBorderIcon />}></Button>
+                <Button variant="text" sx={{ alignSelf: 'start', justifySelf: 'flex-end'}} startIcon={<FavoriteBorderIcon />}></Button>
+            </Box>
+                <Stack sx={{
+                    m: 3,
+                    gap: 2,
+                }}>
+                    {comments.map((comment: any, index: number) => {
+                        if(!comment.body) { return };
+                        return comment.replies ? <Comment comment={comment} replies={comment.replies} key={index} /> : <Comment comment={comment} key={index}/>})}
+                </Stack>
+
             </Box>
         </>
     )
@@ -51,13 +75,11 @@ export default function PostComments(props: { post: any, comments: any }): JSX.E
 
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-    console.log(ctx)
     const { subreddit, cid } = ctx.query;
     const res = await fetch("https://www.reddit.com/r/" + subreddit + `/${cid}.json`)
     const data = await res.json();
     const post = data[0].data.children[0].data
-    const comments = data[1].data.children;
-    comments.forEach(comment => comment.data);
+    const comments = data[1].data.children.map((comment: any) => comment.data);
     return {
         props: {
             post,
